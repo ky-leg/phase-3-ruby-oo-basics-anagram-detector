ec2e93636ccfa520f7b2f9e1dde6e0b091ce09aa
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-02-23 22:05:50 -0500

    commit tree f54094686588729ff6d10e6c1ce2ef19ee1dbae1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word 
+    end
+
+    def order word 
+        word.split("").sort
+    end
+
+    def match array
+        return_array = array.filter {|word| order(word) == order(@word)}
+        puts return_array
+        return_array
+    end
+end
+
+first = Anagram.new("Hello")
+first.match ["Hello", "goodbye", "HELLO", "GOODBYE"]
